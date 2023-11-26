426ba70765eb69d185b3286b1baf47665a664edc
Georg Graf (SPG) <grafg@spengergasse.at>
2023-11-26 15:11:43 +0100

    started the grid

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const fetchLimit = document.getElementById("fetch-limit");
 const fetchBtn = document.getElementById("fetch-btn");
 const fetchStatus = document.getElementById("fetch-status");
 const cancelBtn = document.getElementById("cancel-btn");
-let tmp;
+const gridX = document.getElementById("grid-x");
+const gridY = document.getElementById("grid-y");
+const gritDraw = document.getElementById("grid-draw");
+const gridMain = document.getElementById("main-grid");
+const gridStatus = document.getElementById("grid-status");
+
 async function fetchAllPokes() {
     allPokes = [];
     let limit = fetchLimit.value;
@@ -47,4 +52,28 @@ fetchBtn.addEventListener("click", () => {
 });
 cancelBtn.addEventListener("click", (e) => {
     e.target.controller.abort();
+});
+gritDraw.addEventListener("click", () => {
+    let x = gridX.value;
+    let y = gridY.value;
+    let cell, odd;
+    gridMain.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    gridMain.style.gridTemplateRows = `repeat(${y}, 1fr)`;
+    while (gridMain.firstChild) {
+        gridMain.removeChild(gridMain.firstChild);
+    }
+    for (let i = 0; i < x * y; i++) {
+        cell = document.createElement("div");
+        cell.classList.add("grid-cell");
+        cell.innerHTML = i + 1;
+        gridMain.appendChild(cell);
+    }
+    odd = gridMain.childNodes.length % 2 === 0 ? false : true;
+    if (odd) {
+        gridStatus.innerHTML = `Grid: ${x}x${y} (odd)`;
+        gridStatus.style.color = "red";
+    } else {
+        gridStatus.innerHTML = `Grid: ${x}x${y} (even)`;
+        gridStatus.style.removeProperty("color");
+    }
 });
